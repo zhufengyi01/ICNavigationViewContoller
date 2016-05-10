f61c06efbca83740a4c0a770134032bbb0caaba9
朱封毅 <673229963@qq.com>
2016-05-10 16:40:25 +0800

    Initial Commit

---
+++ ICNavigationViewContoller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ICNavigationViewContoller
+//
+//  Created by 朱封毅 on 10/05/16.
+//  Copyright © 2016年 taihe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ICNavigationViewContoller/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ICNavigationViewContoller
+//
+//  Created by 朱封毅 on 10/05/16.
+//  Copyright © 2016年 taihe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
